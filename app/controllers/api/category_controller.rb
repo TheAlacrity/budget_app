327f3d5ae381f014ed985a58dd9950c4e4a01444
ErbalEssences <LieseErb.com>
2019-06-12 16:37:23 -0500

    changed gem file

--- app/controllers/api/category_controller.rb
+++ app/controllers/api/category_controller.rb
@@ -1,5 +1,5 @@
 class Api::CategoryController < ApplicationController
-  before_action :authenticate_user, only: [:create, :update, :destroy]
+  before_action :authenticate_user
 
   def index
     if current_user
