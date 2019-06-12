a50fafd0542759857f70cbbc1dcc2ff828387bc6
ErbalEssences <LieseErb.com>
2019-06-12 15:24:03 -0500

    added index and create to category

--- app/controllers/api/category_controller.rb
+++ app/controllers/api/category_controller.rb
@@ -1,9 +1,15 @@
 class Api::CategoryController < ApplicationController
+  before_action :authenticate_user, only: [:create, :update, :destroy]
 
   def index
+    if current_user
+      @catagories = current_user.catagories
+      render 'index.json.jbuilder'
+    else
+      @catagories = Catagory.all
+      render json: []
     
   end
-
 
 
   def create
@@ -21,6 +27,35 @@ class Api::CategoryController < ApplicationController
       render json: { errors: @Catagory.errors.full_messages }, status: :unprocessable_entity
     end
   end
+
+  # def show
+  #   @product = Catagory.find(params[:id])
+  #   render 'show.json.jbuilder'
+  # end
+
+  # def update
+  #   @product = Product.find(params[:id])
+  #   @product.name = params[:name] || @product.name
+  #   @product.price = params[:price] || @product.price
+  #   @product.description = params[:description] || @product.description
+  #   @product.material = params[:material] || @product.material
+  #   @product.in_stock = params[:in_stock] || @product.in_stock
+  #   @product.style = params[:style] || @product.style
+  #   @product.supplier_id = params[:supplier_id] || @product.supplier_id
+    
+
+  #   if @product.save
+  #     render 'show.json.jbuilder'
+  #   else 
+  #     render json: {message: @product.errors.full_messages }, status: :unprocessable_entity
+  #   end
+  end
+
+  # def destroy
+  #   @product = Product.find(params[:id])
+  #   @product.destroy
+  #   render json: {message: "Successfully destroyed products"}
+  # end
 
 
 end
