477639dab6dac068c5551b1ce604dfb55da1244c
TheAlacrity <brinker.jeff@gmail.com>
2019-06-12 16:52:40 -0500

    final push for day; incomplete data added to seeds file; category_controller fixed

--- app/controllers/api/category_controller.rb
+++ app/controllers/api/category_controller.rb
@@ -12,13 +12,10 @@ class Api::CategoryController < ApplicationController
 
 
   def create
-    t.string "name"
-    t.decimal "budget", precision: 11, scale: 2
-    t.integer "user_id" 
     @category = Category.new(
                             name: params[:name],
                             budget: params[:budget],
-                            user_id: 1 # current_user.id
+                            user_id: current_user.id
                           )
     if @category.save
       render 'show.json.jbuilder'
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,22 @@
     # t.string "comment"
     # t.integer "user_id"
     # t.integer "category_id"
+User.delete_all
+Expense.delete_all
+Category.delete_all
 
-# expense = Expense.new(name: "Expense 1", amount: 30.99)
-# expense.save
-expense = Expense.new(name: "Expense 2", amount: 25.00)
+user = User.create(name: "Joe", email: "joe@budget.com", password: "password", password_confirmation: "password")
+user = User.create(name: "John", email: "john@budget.com", password: "password", password_confirmation: "password")
+
+
+expense = Expense.new(name: "Expense 1", amount: 30.99, comment: "", user_id: "", category_id: "")
 expense.save
-expense = Expense.new(name: "Expense 3", amount: 9.13)
+expense = Expense.new(name: "Expense 2", amount: 25.00, comment: "", user_id: "", category_id: "")
 expense.save
-expense = Expense.new(name: "Expense 4", amount: 40.00)
+expense = Expense.new(name: "Expense 3", amount: 9.13, comment: "", user_id: "", category_id: "")
 expense.save
-expense = Expense.new(name: "Expense 5", amount: 15.00)
+expense = Expense.new(name: "Expense 4", amount: 40.00, comment: "", user_id: "", category_id: "")
 expense.save
+expense = Expense.new(name: "Expense 5", amount: 15.00, comment: "", user_id: "", category_id: "")
+expense.save
+
